b0950a6aaf5225cfc9afe8d8a1bebb9b7d7c3bd8
AletaHakan <AletaHakan@users.noreply.github.com>
2018-05-18 00:39:58 -0600

    first commit to github of the Telescope Array Article Database website.

---
+++ js/myJS.js
@@ -0,0 +1,8 @@
+
+$('.datepicker').pickadate({
+  // Escape any “rule” characters with an exclamation mark (!).
+  format: 'You selecte!d: 'yyyy, mm, dd',
+  formatSubmit: 'yyyy/mm/dd',
+  hiddenPrefix: 'prefix__',
+  hiddenSuffix: '__suffix'
+})
